27ee17218d7f23ae6a83de5f1caa95fa93d60b94
Samuel <jacksamuel089@gmail.com>
2024-09-25 16:54:20 +0100

    headers

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,8 @@ import logo from "../../assets/logo.svg";
 import Hamburger from 'hamburger-react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { BiMenu } from 'react-icons/bi';
+import { CgClose } from 'react-icons/cg';
 
 
 const Header = () => {
@@ -55,7 +57,7 @@ const Header = () => {
 
   return (
     <header>
-      {isMobile ? (
+      {/* {isMobile ? (
         <div className="header-mobile">
           <CiSearch onClick={()=>navigate('/explore-campaigns')}/>
           <img src={logo} alt="logo" className="header-logo" onClick={{proceed}} />
@@ -91,7 +93,35 @@ const Header = () => {
             <a className="signup-button" onClick={() => navigate('/signup')}>Sign Up</a>
           </div>
         </div>
-      )}
+      )} */}
+      <div className='headWrapper'>
+        <div className='headCampBox' onClick={()=>navigate('/explore-campaigns')}>
+          Campaigns
+        </div>
+        <div className='headLogoBox'>
+          <img src={logo} alt="" onClick={()=>navigate('/')} />
+        </div>
+        <div>
+          <ul>
+            <li onClick={()=>navigate('/about-us')}>About</li>
+            <li onClick={()=>navigate('/login')}>Login</li>
+            <li><button onClick={()=>navigate('/signup')}>sign up</button></li>
+            <div className='mobileMenuHead'>{isOpen ? <CgClose color='white' onClick={()=>setIsOpen(false)} size={20}/>:<BiMenu  onClick={()=>setIsOpen(true)} size={20}/>}</div>
+          </ul>
+          {
+            isOpen ? <div className='mediaHead'>
+              <div className='mediaHeadWrapper'>
+                <nav>
+                  <a href="" onClick={()=>navigate('/explore-campaigns')}>Campaign</a>
+                  <a href="" onClick={()=>navigate('/about-us')}>About</a>
+                  <a href="" onClick={()=>navigate('/login')}>Login</a>
+                  <a href="" onClick={()=>navigate('/signup')}>Signup</a>
+                </nav>
+              </div>
+            </div>:null
+          }
+        </div>
+      </div>
     </header>
   );
 };
